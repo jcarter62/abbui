9fc98b9bb18c60ac285123d1195b67ab3e9e3eda
Jim Carter <jcarter62@gmail.com>
2020-01-22 11:54:15 -0800

    updated startup command file.

--- start.cmd
+++ start.cmd
@@ -1,3 +1,7 @@
 c:
 cd \apps\abbui-app
+SET SESSION_COOKIE_NAME=abbui-app
+SET SESSION_HOST=localhost
+SET SESSION_PORT=27017
+SET SESSION_DB=session
 .\venv\scripts\waitress-serve --host=0.0.0.0 --port=6020 app:app
