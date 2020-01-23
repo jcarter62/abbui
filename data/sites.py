c6677b99806f40d454a86698f8bba11d27fd5fbe
Jim Carter <jcarter62@gmail.com>
2020-01-23 08:06:16 -0800

    worked on "favorites" feature

--- data/sites.py
+++ data/sites.py
@@ -1,5 +1,6 @@
 from .settings import Settings
 import requests, json
+import base64
 
 class Sites:
 
@@ -10,6 +11,13 @@ class Sites:
         self.total_flow = 0.0
         self.mrr_flow = 0.0
         self.get_sites()
+        return
+
+    def make_id(self, name):
+        bytes_array = str.encode(name)
+        encoded = base64.b16encode(bytes_array)
+        result = encoded.decode('utf-8')
+        return result
 
     def get_sites(self):
         url = self.base_url + '/api/uihome'
@@ -23,6 +31,12 @@ class Sites:
             self.sites = jdata['sites']
             self.total_flow = jdata['totalflow']
             self.mrr_flow = jdata['mrrflow']
+            #
+            # Add row ID
+            #
+            for s in self.sites:
+                s['id'] = self.make_id(s['dispname'])
+
         except requests.exceptions.RequestException as e:
             print('/api/uihome exception:' + str(e))
 
